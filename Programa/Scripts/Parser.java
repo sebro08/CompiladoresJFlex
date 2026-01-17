b03319c2455c47b1415ddbe20b2f6e714aa6d543
braslyvm <braslymorales@gmail.com>
2026-01-17 17:39:51 -0600

    Pruebas de compilacion

--- Programa/Scripts/Parser.java
+++ Programa/Scripts/Parser.java
@@ -523,7 +523,7 @@ public class Parser extends java_cup.runtime.lr_parser {
             return super.parse();
         } finally {
             if (errores > 0) {
-                System.out.println("\nEl archivo fuente no se puede generar: tiene " + errores + " error(es) sintáctico(s).");
+                System.out.println("\nEl archivo fuente no se puede generar: tiene " + errores + " error sintáctico.");
             }
         }
     }
